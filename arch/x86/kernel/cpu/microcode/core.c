5babfcfea75c9ceb1c73a52a67a194f1b33cb199
Ashok Raj <ashok.raj@intel.com>
2023-06-06 13:19:17 -0700

    x86/microcode: Only update cpuinfo for sibling CPUs.
    For Sibling CPUs, only update the revision values in cpuinfo_x86. Do not
    call apply_microcode() since the lead thread has already updated microcode.
    
    Signed-off-by: Ashok Raj <ashok.raj@intel.com>

--- arch/x86/kernel/cpu/microcode/core.c
+++ arch/x86/kernel/cpu/microcode/core.c
@@ -446,13 +446,12 @@ wait_for_siblings:
 		panic("Timeout during microcode update!\n");
 
 	/*
-	 * At least one thread has completed update on each core.
-	 * For others, simply call the update to make sure the
-	 * per-cpu cpuinfo can be updated with right microcode
-	 * revision.
+	 * The lead thread has completed update on each core.
+	 * For others, simply update the per-cpu cpuinfo
+	 * with microcode revision.
 	 */
 	if (!lead_thread)
-		err = apply_microcode(cpu);
+		update_cpuinfo_x86(cpu);
 
 	return ret;
 }
