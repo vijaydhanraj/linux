a481ee3948c1a58cce19d4bda03b33c364780610
Ashok Raj <ashok.raj@intel.com>
2023-06-06 13:03:06 -0700

    x86/microcode: Introduce common function to update cpuinfo_x86
    Add a new function to update cpuinfo_x86 ucode specific information.
    
    Add a separate function, so that could be used outside of the
    apply_microcode() for sibling CPUs.
    
    Suggested-by: Dave Hansen <dave.hansen@intel.com>
    Signed-off-by: Ashok Raj <ashok.raj@intel.com>

--- arch/x86/kernel/cpu/microcode/core.c
+++ arch/x86/kernel/cpu/microcode/core.c
@@ -378,20 +378,25 @@ static int __wait_for_cpus(atomic_t *t, long long timeout)
 	return 0;
 }
 
-static enum ucode_state apply_microcode(int cpu)
+static void update_cpuinfo_x86(int cpu)
 {
 	struct cpuinfo_x86 *c = &cpu_data(cpu);
-	enum ucode_state err;
 	bool bsp;
 
 	bsp = c->cpu_index == boot_cpu_data.cpu_index;
-
-	err = microcode_ops->apply_microcode(cpu);
 	c->microcode = microcode_ops->get_current_rev();
 
 	/* Update boot_cpu_data's revision too, if we're on the BSP: */
 	if (bsp)
 		boot_cpu_data.microcode = c->microcode;
+}
+
+static enum ucode_state apply_microcode(int cpu)
+{
+	enum ucode_state err;
+
+	err = microcode_ops->apply_microcode(cpu);
+	update_cpuinfo_x86(cpu);
 
 	return err;
 }
